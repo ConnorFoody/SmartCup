7a222698f2ac7e0bc01603dad5347f28dd82f597
ConnorFoody <connorfoody@gmail.com>
2014-10-05 09:46:57 -0400

    Finished everything off, need to tidy things up a little but are otherwise good

--- app/src/main/java/com/example/connorfoody/smartcup/MainBasic.java
+++ app/src/main/java/com/example/connorfoody/smartcup/MainBasic.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 import android.app.Notification.*;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
@@ -39,11 +40,10 @@ public class MainBasic extends Activity {
 
         times_read = 0;
 
-        predict_series = new GraphViewSeries(new GraphViewData[] {});
         data_series = new GraphViewSeries(new GraphViewData[] {});
         goal_series = new GraphViewSeries(new GraphViewData[] {});
-        goal_series.getStyle().color = Color.RED;
-        data_series.getStyle().color = Color.BLUE;
+        goal_series.getStyle().color = Color.GREEN;
+        data_series.getStyle().color = Color.RED;
 
 
         // set up the graph
@@ -61,24 +61,16 @@ public class MainBasic extends Activity {
 
         graph.getGraphViewStyle().setNumHorizontalLabels(5);
         graph.getGraphViewStyle().setGridStyle(GraphViewStyle.GridStyle.VERTICAL);
+        graph.setManualYMinBound(0);
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.graph);
         layout.addView(graph);
 
-        m_notification = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!").build();
-        m_notification_manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
     }
-    private GraphViewSeries predict_series;
     private GraphViewSeries data_series;
     private GraphViewSeries goal_series;
     private GraphView graph;
     private int times_read;
-    Notification m_notification;
-    NotificationManager m_notification_manager;
     // simple handler to help with debugging
     public Handler debug_handler = new Handler(){
         @Override
@@ -101,16 +93,16 @@ public class MainBasic extends Activity {
         public void handleMessage(Message msg){
             if(msg.what == 1){
                 SCMessage m_msg = (SCMessage)msg.obj;
-                if(m_msg.state == TemperatureProcessor.max){
+                if(times_read%60 == 0 || (m_msg.temp < 137 && times_read > 10)){
+                    if(m_msg.predict != 0) {
+                        Toast.makeText(getApplicationContext(), "" + Math.round(m_msg.predict / 60) + ":" + Math.round(m_msg.predict % 60) + " until ideal temperature", Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "drink is at safe temperature", Toast.LENGTH_LONG).show();
+                    }
+                }
+                data_series.appendData(new GraphViewData((double)times_read , (double)Math.round(m_msg.temp * 10.0) / 10.0 ), true, 200);
+                goal_series.appendData(new GraphViewData((double)times_read , 136), true, 200);
 
-                }
-                else if(m_msg.state == TemperatureProcessor.decreasing){
-                }
-                if(times_read == 50){
-                    m_notification_manager.notify(1, m_notification);
-                }
-                data_series.appendData(new GraphViewData((double)times_read * 0.5, (double)Math.round(m_msg.temp * 10.0) / 10.0 ), true, 100);
-                goal_series.appendData(new GraphViewData((double)times_read * 0.5, 136), true, 100);
                 times_read++;
             }
             else if(msg.what == -1){
